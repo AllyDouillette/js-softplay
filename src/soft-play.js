0e8a2879a40919a3cbe8b6db88c607c46413eb7d
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-04-07 14:33:56 +0200

    formatting

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,43 +1,43 @@
 // do not change these lines
-let adults = 0
-let children = 0
+const adults = 0
+const children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
 // first we initialize the variables that keep track of the adults and kids
-let numAdults = 0;
-let numChildren = 0;
+const numAdults = 0;
+const numChildren = 0;
 
 // here is what the object of the softballCenter looks like
-let softballCenter = {
+const softballCenter = {
   "adults": numAdults,
   "children": numChildren
 }
 
 // here's the tracker that tracks the total
-let softballCenterTotal = {
+const softballCenterTotal = {
   "adults": 0,
   "children": 0,
 }
 
 // function that basically only prints out the current state of the softball-Center-object
-const occupancy = (() => softballCenter)
-const total = (() => softballCenterTotal)
+const occupancy = () => softballCenter
+const total = () => softballCenterTotal
 
 //adding shit so i can easily reset them – not necessarily used
-const resetOccupancy = (() => {
+const resetOccupancy = () => {
   softballCenter.adults = 0,
   softballCenter.children = 0
-})
+}
 
-const resetTotalCount = (() => {
+const resetTotalCount = () => {
   softballCenterTotal.adults = 0,
   softballCenterTotal.children = 0
-})
+}
 
-enter = (num1, num2) => {
+const enter = (num1, num2) => {
   if (num1 >= num2) {
     softballCenter.adults += num1
     softballCenter.children += num2
@@ -49,7 +49,7 @@ enter = (num1, num2) => {
   }
 }
 
-leave = (num1, num2) => {
+const leave = (num1, num2) => {
   if (num1 >= num2 && softballCenter.adults - num1 >= softballCenter.children - num2) {
     softballCenter.adults -= num1
     softballCenter.children -= num2
